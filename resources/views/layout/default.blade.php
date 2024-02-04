f2ab47b81af12dc68bb6849ace192a3749b377b5
SamieQamar <93860180+Samie-Qamar@users.noreply.github.com>
2024-02-04 15:46:38 +0500

    theme steup

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>srtdash - ICO Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('include.head')
+</head>
+
+<body>
+@include('include.header')
+@yield('content')
+<div id="preloader">
+<div class="loader"></div>
+</div>
+<footer>
+
+</footer>
+</body>
+@include('include.footerscripts')
+</html>
