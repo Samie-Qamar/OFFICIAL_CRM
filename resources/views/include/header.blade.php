4cfbd19a3d80da8347ad712caa22ee8398b14a97
SamieQamar <93860180+Samie-Qamar@users.noreply.github.com>
2024-11-02 00:38:54 +0500

    commits

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,76 +1,57 @@
 <div class="page-container">
-        <!-- sidebar menu area start -->
-        <div class="sidebar-menu">
-            <div class="sidebar-header">
-              
-            </div>
-            <div class="main-menu">
-                <div class="menu-inner">
-                    <nav>
-                        <ul class="metismenu" id="menu">
-                            <li class="active">
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>STAFF MODULE</span></a>
-                                <ul class="collapse">
-                                    <li class="active"><a href="{{route('manage.all.leave')}}">Manage Leaves</a></li>
-                                    <li class="active"><a href="{{route('manage.all.holiday')}}">Manage Annoucement</a></li>
-                                    {{--<li><a href="index2.html">Ecommerce dashboard</a></li>
-                                    <li><a href="index3.html">SEO dashboard</a></li>--}}
-                                </ul>
-                            </li>
-                            {{-- <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Sidebar
-                                        Types
-                                    </span></a>
-                                <ul class="collapse">
-                                   {{-- <li><a href="index.html">Left Sidebar</a></li>
-                                    <li><a href="index3-horizontalmenu.html">Horizontal Sidebar</a></li>-}}
-                                </ul>
-                            </li> --}}
-                            
-                    
-                            {{-- <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-slice"></i><span>icons</span></a>
-                                <ul class="collapse">
-                                    <li><a href="fontawesome.html">fontawesome icons</a></li>
-                                    <li><a href="themify.html">themify icons</a></li>
-                                </ul>
-                            </li> --}}
-                            {{-- <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i>
-                                    <span>Tables</span></a>
-                                <ul class="collapse">
-                                    <li><a href="table-basic.html">basic table</a></li>
-                                    <li><a href="table-layout.html">table layout</a></li>
-                                    <li><a href="datatable.html">datatable</a></li>
-                                </ul>
-                            </li> --}}
-                            {{-- <li><a href="maps.html"><i class="ti-map-alt"></i> <span>maps</span></a></li>
-                            
-                                <li><a href="invoice.html"><i class="ti-receipt"></i> <span>Invoice Summary</span></a></li>
- --}}                          
- 
-                            {{-- <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layers-alt"></i> <span>Pages</span></a>
-                                <ul class="collapse">
-                                    <li><a href="login.html">Login</a></li>
-                                    <li><a href="login2.html">Login 2</a></li>
-                                    <li><a href="login3.html">Login 3</a></li>
-                                    <li><a href="register.html">Register</a></li>
-                                    <li><a href="register2.html">Register 2</a></li>
-                                    <li><a href="register3.html">Register 3</a></li>
-                                    <li><a href="register4.html">Register 4</a></li>
-                                    <li><a href="screenlock.html">Lock Screen</a></li>
-                                    <li><a href="screenlock2.html">Lock Screen 2</a></li>
-                                    <li><a href="reset-pass.html">reset password</a></li>
-                                    <li><a href="pricing.html">Pricing</a></li>
-                                </ul>
-                            </li> --}}
-                          
-                           
-                          
-                        </ul>
-                    </nav>
-                </div>
+    <!-- sidebar menu area start -->
+    <div class="sidebar-menu">
+        <div class="sidebar-header">
+            <!-- Optional: Add a logo or title here -->
+        </div>
+        <div class="main-menu">
+            <div class="menu-inner">
+                <nav>
+                    <ul class="metismenu" id="menu">
+                        <li class="active">
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Dashboard</span></a>
+                            <ul class="collapse">
+                                <li class="active"><a href="{{ route('manage.all.leave') }}">Manage Leaves</a></li>
+                                <li><a href="{{ route('manage.all.holiday') }}">Manage Announcements</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li>
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-slice"></i><span>Icons</span></a>
+                            <ul class="collapse">
+                                <li><a href="fontawesome.html">FontAwesome Icons</a></li>
+                                <li><a href="themify.html">Themify Icons</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i><span>Tables</span></a>
+                            <ul class="collapse">
+                                <li><a href="table-basic.html">Basic Table</a></li>
+                                <li><a href="table-layout.html">Table Layout</a></li>
+                                <li><a href="datatable.html">DataTable</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layers-alt"></i><span>Pages</span></a>
+                            <ul class="collapse">
+                                <li><a href="login.html">Login</a></li>
+                                <li><a href="register.html">Register</a></li>
+                                <li><a href="reset-pass.html">Reset Password</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li>
+                            <a href="maps.html"><i class="ti-map-alt"></i><span>Maps</span></a>
+                        </li>
+                        
+                        <li>
+                            <a href="invoice.html"><i class="ti-receipt"></i><span>Invoice Summary</span></a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
-`
+    </div>
+</div>
